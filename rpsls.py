1e1af05d778a109fe5392c54dfb1f17df42265d9
Samantha Roach <sam@roach-uk.com>
2015-09-16 14:25:12 +0200

    Added interactive buttons to the game

--- rpsls.py
+++ rpsls.py
@@ -10,9 +10,29 @@
 # 3 - lizard
 # 4 - scissors
 
-# helper functions
+# input will come from buttons 
+# all output for the game will be printed in the console
 
+
+import simplegui
 import random
+
+player_score = 0
+comp_score = 0
+count = 0
+
+# helper function to start and restart the game
+
+def new_game():
+    global comp_score
+    global player_score
+    global count
+    comp_score = 0
+    player_score = 0
+    count = 0
+    print
+    print 'choose between: rock, paper, scissors, lizard or Spock'
+    print
 
 def name_to_number(name):
     """
@@ -49,50 +69,85 @@ def number_to_name(number):
         return 'scissors'
     else:
         return 'That number is incorrect'
+    
+def best_of_3(comp_score, player_score):
+    if comp_score > player_score:
+        print
+        print 'Sorry the computer beat you!'
+    else:    
+        print
+        print 'Congratulations! you won best out of 3!'
+        
+    
 
-def rpsls(player_choice): 
-    """Plays player_choice against randomly selected computer 
-    choice and prints the winner
-    """       
-    print #a blank line to separate consecutive games
+# define event handlers for control panel
 
-    # print out the message for the player's choice
+
+def guess_rock():
+    return input_guess('rock')
+
+def guess_paper():
+    return input_guess('paper')
+
+def guess_scissors():
+    return input_guess('scissors')
+
+def guess_lizard():
+    return input_guess('lizard')
+
+def guess_spock():
+    return input_guess('Spock')
+    
+def input_guess(player_choice):
+    global comp_score
+    global player_score
+    global count
+    count += 1
+    print
+    print 'Game', count
+    print
     print 'Player chooses: ', player_choice
-
-    # convert the player's choice to player_number using the function name_to_number()
     player_number = name_to_number(player_choice)
-
-    # compute random guess for comp_number using random.randrange()
+    player_number = 0
     comp_number = random.randrange(5)
-
-    # convert comp_number to comp_choice using the function number_to_name()
     comp_choice = number_to_name(comp_number)
-    
-    # print out the message for computer's choice
     print 'Computer chooses: ', comp_choice
-
-    # compute difference of comp_number and player_number modulo five
     difference = (player_number - comp_number) %5
-    
-
-    # use if/elif/else to determine winner, print winner message
+# use if/elif/else to determine winner, print winner message
     if difference ==2 or difference ==1:
         print 'Player wins!'
+        player_score += 1
     elif difference ==3 or difference ==4:
         print 'Computer wins!'
+        comp_score += 1
     elif difference ==0:
         print 'player and computer tie!'
+        print    
+    if count == 3:
+        best_of_3(comp_score, player_score)
+        comp_score = 0
+        player_score = 0
+        count = 0
+        
+   
+  
     
     
-    
-# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
-rpsls("rock")
-rpsls("Spock")
-rpsls("paper")
-rpsls("lizard")
-rpsls("scissors")
-
-# always remember to check your completed program against the grading rubric
+        
+# create frame
+frame = simplegui.create_frame('Testing', 300, 200)
 
 
+# register event handlers for control elements and start frame
+
+frame.add_button('rock', guess_rock, 100)
+frame.add_button('paper', guess_paper, 100)
+frame.add_button('lizard', guess_lizard, 100)
+frame.add_button('scissors', guess_scissors, 100)
+frame.add_button('spock', guess_spock, 100)
+frame.add_button('new game', new_game, 100)
+
+
+# call new_game 
+new_game()
 
